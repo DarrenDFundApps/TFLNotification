e1c43656c4a27852aa1a8d88497dd4468d9e890d
Darren Daley <168552125+DarrenDFundApps@users.noreply.github.com>
2024-05-14 11:12:47 +0100

    Added Domain project and class

---
+++ TFLNotifications.Domain/StationStatusModel.cs
@@ -0,0 +1,36 @@
+﻿namespace TFLNotifications.Domain;
+
+public enum SeverityStatus
+{
+    Minor,
+    Major
+}
+
+public enum Lines
+{
+    Bakerloo,
+    Central,
+    Circle,
+    District,
+    HammersmithAndCity,
+    Jubilee,
+    Metropolitan,
+    Northern,
+    Piccadilly,
+    Victoria,
+    WaterlooAndCity,
+}
+
+public class StationStatusModel(
+    string stationName,
+    SeverityStatus status,
+    string description,
+    DateTime fromDate,
+    Lines line)
+{
+    public string StationName { get; init; } = stationName;
+    public SeverityStatus Status { get; init; } = status;
+    public string Description { get; init; } = description;
+    public DateTime FromDate { get; init; } = fromDate;
+    public Lines Line { get; init; } = line;
+}
