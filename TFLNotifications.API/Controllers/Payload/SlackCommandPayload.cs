fc91534be58e3f7628fb5afc550ebb377a948c22
Hayley Kwok <hayley@fundapps.co>
2024-05-14 15:35:49 +0100

    Add new controller to handle what slack send us back (#2)
    * Add the new controller for getting the status from the database
    
    * add subscribe controller
    
    * handling what slack sent back from the slash command

---
+++ TFLNotifications.API/Controllers/Payload/SlackCommandPayload.cs
@@ -0,0 +1,6 @@
+﻿namespace TFLNotifications.API.Controllers.Payload;
+
+public record SlackCommandPayload(string UserId, string Username, string Text, string RequestUrl)
+{
+
+}
---
+++ TFLNotifications.API/Controllers/SubscribeController.cs
@@ -0,0 +1,24 @@
+﻿using System.Net;
+using System.Text;
+using Microsoft.AspNetCore.Mvc;
+using TFLNotifications.API.Controllers.Payload;
+
+namespace TFLNotifications.API.Controllers;
+
+[Route("subscribe")]
+[ApiController]
+public class SubscribeController : ControllerBase
+{
+
+
+    [HttpPost]
+    public async Task<IActionResult> PostSlack()
+    {
+        var rawRequestBody = await Request.GetRawBodyAsync();
+        var payload = SlackCommandHelper.ExtractData(rawRequestBody);
+        Console.WriteLine($"Raw request:{rawRequestBody}");
+        Console.WriteLine($"Payload:{payload}");
+
+        return  Ok();
+    }
+}
